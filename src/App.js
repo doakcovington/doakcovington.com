d17f8d2f69f9062ce4bdc464b24b4e860dbb68b7
doakcovington <doakcovington@protonmail.com>
2020-09-24 12:27:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          We Don't Go To Ravenholm
         </a>
       </header>
     </div>
